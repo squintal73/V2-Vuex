56a68a9a7cd35a33d050c0b62d9c79cd2d0fb84c
squintal73 <sidnei_quintal@yahoo.com.br>
2022-07-27 01:20:40 -0300

    Conf. Vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+<h1>Counter: {{ counter }}</h1>
+<button @click="decrement">Decremente</button>
+<button @click="increment">Incremente</button>
+</div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default{
+    name: 'counter-j',
+    computed:{
+        ...mapState({
+            counter:state => state.counter
+        })     
+    },
+    methods: {
+       ...mapMutations([
+            'decrement',
+            'increment'
+
+       ])
+    }
+  }
+
+</script>
